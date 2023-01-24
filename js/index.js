b9554334fa2761766c6b2de9b8afc8b2b1f38152
vdevcode <thanhvumaidao123456@gmail.com>
2023-01-24 19:41:39 +0700

    upload

--- js/index.js
+++ js/index.js
@@ -803,3 +803,20 @@ const imagePaths = [
   });
 }
 openImageSocials()
+
+
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Exit Fullscreen";
+    }
+    else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Go Fullscreen";
+    }
+});
